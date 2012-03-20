15aa0ffbc5356e8de9a29d3651346b5c4aaee042
Stephen Clouse <stephenclouse@gmail.com>
2012-03-19 20:25:27 -0500

    Hide test module from PAUSE

--- corpus/dist/lib/DZ1.pm
+++ corpus/dist/lib/DZ1.pm
@@ -1,10 +1,9 @@
-use strict;
-use warnings;
-package DZ1;
+package # hide from PAUSE
+    DZ1;
 # ABSTRACT: this is a sample package for testing Dist::Zilla;
 
 sub main {
-  return 1;
+    return 1;
 }
 
 1;
